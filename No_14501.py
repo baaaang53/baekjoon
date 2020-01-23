797a1bc97b12557c2e35a579e289971c7944a6ce
bangcoder <57452304+baaaang53@users.noreply.github.com>
2020-01-24 00:12:04 +0900

    퇴사 _완성
    백준 #14501 _ 퇴사 _ dynamic programming _ 완성
    
    - max를 변수명으로 사용해 max함수가 제 기능을 하지 못 함. 
    - 퇴사 날짜를 넘어가는 상담에 대해서도 해당 상담을 선택하지 않았을 경우 얻을 수 있는 비용을 저장해야 함. - 그 상담 후에 있는 어떤 상담이 수익을 낼 수 있기 때문.

--- No_14501.py
+++ No_14501.py
@@ -3,7 +3,7 @@ table = [[0,0]]
 #price[i][0] : i를 선택했을 때 최대 비용
 #price[i][1] : i를 선택하지 않았을 때의 최대 비용
 price = [[0,0] for i in range(0,N+1)]
-max = 0
+maxi = 0
 
 for i in range(0,N) :
     inp = input().split()
@@ -19,13 +19,15 @@ for i in range(N,0,-1) :
     # 마지막날보다 먼저 끝난다면
     elif ((i + table[i][0]) < N+1):
         next = i + table[i][0]
-        #price[i][0] = table[i][1] + price[i + table[i][0]][0]
-        price[i][0] = table[i][1] + price[next][0]
+        price[i][0] = table[i][1] + max(price[next][0],price[next][1])
+
+    if (i != N) :
+        price[i][1] = max(price[i + 1][0], price[i + 1][1])
 
 
 for i in range(1,N+1) :
-    if max < price[i][0] :
-        max = price[i][0]
+    if maxi < price[i][0] :
+        maxi = price[i][0]
 
-print(max)
+print(maxi)
 
