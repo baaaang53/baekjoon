46bc7670322af6068ea18717917569738f9cd4bf
bangcoder <57452304+baaaang53@users.noreply.github.com>
2020-02-02 21:04:12 +0900

    단지번호 붙이기
    백준 #2667 _ 단지번호 붙이기 _ 재귀 말고 bfs/dfs로 하면 더 수월함.

--- No_2667.py
+++ No_2667.py
@@ -2,38 +2,48 @@ N = int(input())
 
 matrix = [] # 입력받은 지도 정보 저장
 check = [[0]*N for i in range(0,N)] # 해당 집을 체크했는지 저장. 1은 체크 함, 0은 안함.
-housecount = [0] # 번지수에 해당하는 집의 수 저장
+housecount = [] # 번지수에 해당하는 집의 수 저장
 counting = 0 # 집 몇갠지 세는 용도
+queue = []
 
 for i in range(0,N) :
     inp = list(input())
     inp = [int(i) for i in inp]
     matrix.append(inp)
 
+def isEmpty(list) :
+    return (len(list) == 0)
 
-def findneighbor(x,y) :
-    check[x][y] = 1 # 집 방문.
-    counting =1
+def findneighbor(a,b) :
+    queue.append([a,b])
+    counting =0
 
-    #북
-    if (x != 0) :
-       if (matrix[x-1][y] == 1 and check[x-1][y] == 0) :
-           counting += findneighbor(x-1, y)
+    while (isEmpty(queue) == False) :
+        [x,y] = queue.pop(0)
 
-    #동
-    if (y != N-1) :
-        if (matrix[x][y+1] == 1 and check[x][y+1] == 0) :
-            counting += findneighbor(x, y+1)
+        if check[x][y] == 0 :
+            check[x][y] = 1
+            counting += 1
 
-    #남
-    if (x != N-1) :
-        if (matrix[x+1][y] == 1 and check[x+1][y] == 0) :
-            counting += findneighbor(x+1, y)
+            #북
+            if (x != 0) :
+               if (matrix[x-1][y] == 1 and check[x-1][y] == 0) :
+                   queue.append([x-1,y])
 
-    #서
-    if (y != 0) :
-        if (matrix[x][y-1] == 1 and check[x][y-1] == 0) :
-            counting += findneighbor(x, y-1)
+            #동
+            if (y != N-1) :
+                if (matrix[x][y+1] == 1 and check[x][y+1] == 0) :
+                    queue.append([x,y+1])
+
+            #남
+            if (x != N-1) :
+                if (matrix[x+1][y] == 1 and check[x+1][y] == 0) :
+                    queue.append([x+1,y])
+
+            #서
+            if (y != 0) :
+                if (matrix[x][y-1] == 1 and check[x][y-1] == 0) :
+                    queue.append([x,y-1])
 
     return counting
 
@@ -45,6 +55,7 @@ for i in range(0,N) :
             num = findneighbor(i,j)
             housecount.append(num)
 
-print(len(housecount)-1)
-for k in range(1,len(housecount)) :
-    print(housecount[k])
+print(len(housecount))
+housecount.sort()
+for k in housecount :
+    print(k)
