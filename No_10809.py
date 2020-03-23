97288aac0b9f6520b092dd148e7259e6d11d6cf5
bangcoder <57452304+baaaang53@users.noreply.github.com>
2020-03-23 22:37:53 +0900

    알파벳 찾기

---
+++ No_10809.py
@@ -0,0 +1,12 @@
+#알파벳 찾기
+
+alpha = [-1] * 26
+
+word = list(input())
+
+for i in range(len(word)) :
+    asc = ord(word[i])
+    if alpha[asc-97] == -1 :
+        alpha[asc - 97] = i
+
+print(alpha)
